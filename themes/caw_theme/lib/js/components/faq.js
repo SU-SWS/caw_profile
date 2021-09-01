acf0e28c4d7157abbf9d896bb50ab7e042cf5de3
Mike Decker <pookmish@gmail.com>
2021-09-01 16:15:48 -0700

    FAQ Component with accordions

---
+++ themes/caw_theme/lib/js/components/faq.js
@@ -0,0 +1,38 @@
+(function ($, Drupal) {
+  Drupal.behaviors.cawFaq = {
+    attach: function attach(context) {
+      $('.ptype-stanford-faq', context).once('faq-expand-all').each((index, faq) => {
+        const $details = $('details', faq);
+        if ($details.length < 2) {
+          return;
+        }
+
+        const $button = $(
+          '<button class="expand-collapse-button expand-all su-button--secondary">' +
+          '<span class="expand-collapse">Expand</span> All' +
+          '<span class="visually-hidden"> Items below.</span>' +
+          '</button>'
+        );
+        $button.click(function () {
+          $button.toggleClass('expand-all').toggleClass('collapse-all');
+          const expanded = !$button.hasClass('expand-all')
+
+          $('span', $button).text(expanded ? 'Collapse' : 'Expand');
+          $details.each((i, detail) => {
+            $(detail).attr('open', expanded);
+            $('summary', detail).attr('aria-expanded', expanded)
+              .attr('aria-pressed', expanded);
+          })
+        })
+
+        const $headline = $('.su-faq-headline', faq);
+        if ($headline.length) {
+          $headline.append($('<div class="button-wrapper">').append($button));
+        }
+        else {
+          $(faq).prepend($('<div class="button-wrapper clearfix">').append($button));
+        }
+      })
+    }
+  };
+})(jQuery, Drupal);

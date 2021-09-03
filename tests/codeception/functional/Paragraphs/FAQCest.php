b965d540d8e5d3bd5b2eea56297a06f41ad8eb1b
Mike Decker <pookmish@gmail.com>
2021-09-03 14:59:42 -0700

    Finished the acceptance test

--- tests/codeception/functional/Paragraphs/FAQCest.php
+++ tests/codeception/functional/Paragraphs/FAQCest.php
@@ -8,7 +8,7 @@
 class FAQCest {
 
   /**
-   * @group testme
+   * FAQ lists should display with a button.
    */
   public function testFaq(FunctionalTester $I) {
     $I->logInWithRole('contributor');
@@ -28,17 +28,14 @@ class FAQCest {
     $I->fillField('Headline', 'FAQ Headliner');
     $I->fillField('Title/Question', 'Did you hear about the guy who invented the knock-knock joke?');
     $I->scrollTo('input[value="Add Accordion"]');
-    $I->wait(5);
     $I->click('Source', '.field--name-su-faq-questions');
-    $I->fillField('.field--name-su-accordion-body textarea', 'He won the “no-bell” prize.');
-
-    //    $I->fillField('Body/Answer', 'He won the “no-bell” prize.');
+    $I->fillField('.field--name-su-accordion-body textarea.cke_source', 'He won the “no-bell” prize.');
 
     $I->click('Add Accordion');
     $I->waitForElement('[name="su_faq_questions[1][subform][su_accordion_title][0][value]"]');
     $I->fillField('Title/Question', 'What do you call a fake noodle?');
     $I->click('Source', '.field--name-su-faq-questions');
-    $I->fillField('Body/Answer', 'An impasta');
+    $I->fillField('.field--name-su-accordion-body textarea.cke_source', 'An impasta');
 
     $I->click('Save', '.ui-dialog-buttonpane');
     $I->waitForElementNotVisible('.ui-dialog');
@@ -47,8 +44,14 @@ class FAQCest {
     $I->canSee('FAQs', 'h1');
     $I->canSee('FAQ Headliner', 'h2');
     $I->canSee('the knock-knock joke', 'details');
-    $I->canSee('“no-bell” prize', 'summary');
     $I->canSee('Expand All', 'button');
+    $I->cantSeeElement('details[open]');
+
+    $I->click('Expand All');
+    $I->canSee('“no-bell” prize');
+    $I->canSee('An impasta');
+    $I->canSee('Collapse All', 'button');
+    $I->canSeeElement('details[open]');
   }
 
 }

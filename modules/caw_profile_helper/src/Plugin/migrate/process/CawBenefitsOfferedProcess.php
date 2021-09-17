ba5f2a15cb55822e36d717553338f132fb16cd7a
Mike Decker <pookmish@gmail.com>
2021-09-17 10:58:14 -0700

    finished benefits and dental imports

---
+++ modules/caw_profile_helper/src/Plugin/migrate/process/CawBenefitsOfferedProcess.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\caw_profile_helper\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateSkipProcessException;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * Provides a 'CawBenefitsProcess' migrate process plugin.
+ *
+ * @MigrateProcessPlugin(
+ *  id = "caw_benefits_offered"
+ * )
+ */
+class CawBenefitsOfferedProcess extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    if (empty($value)) {
+      throw new MigrateSkipProcessException('empty value');
+    }
+    if (strpos($value, 'Active employees') !== FALSE) {
+      return 'active';
+    }
+    if (strpos($value, 'Early retirees') !== FALSE) {
+      return 'early_retirees';
+    }
+    return 'retirees';
+  }
+
+}

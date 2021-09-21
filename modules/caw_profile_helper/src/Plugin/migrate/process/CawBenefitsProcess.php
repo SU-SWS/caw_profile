ae0053c0491f4b3a5297d08f36d636f4f8b4fc33
pookmish <pookmish@stanford.edu>
2021-09-21 12:13:26 -0700

    CAW21-85 Benefits content type

---
+++ modules/caw_profile_helper/src/Plugin/migrate/process/CawBenefitsProcess.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\caw_profile_helper\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateSkipProcessException;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * Provides a 'CawBenefitsProcess' migrate process plugin.
+ *
+ * @MigrateProcessPlugin(
+ *  id = "caw_benefits"
+ * )
+ */
+class CawBenefitsProcess extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    if (empty($value)) {
+      throw new MigrateSkipProcessException('empty value');
+    }
+    $replacements = [
+      '/>[ ]+?</' => '><',
+      '/<p.*?>/' => '',
+      '/<\/p>/' => PHP_EOL,
+      '/\t/' => ' ',
+    ];
+    $value = preg_replace(array_keys($replacements), $replacements, (string) $value);
+    $value = preg_replace('/  +/', ' ', trim(strip_tags($value)));
+    return $value;
+  }
+
+}

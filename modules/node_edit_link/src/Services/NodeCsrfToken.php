579999af206b8947f30703d27bcf1f6752fde1bd
Mike Decker <pookmish@gmail.com>
2021-10-13 15:38:44 -0700

    start a session when the user lands on the form and clear the session when they leave

--- modules/node_edit_link/src/Services/NodeCsrfToken.php
+++ modules/node_edit_link/src/Services/NodeCsrfToken.php
@@ -7,9 +7,8 @@ use Drupal\Core\Access\CsrfTokenGenerator;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Mail\MailManagerInterface;
-use Drupal\Core\Messenger\MessengerInterface;
-use Drupal\Core\Messenger\MessengerTrait;
 use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\Core\Session\SessionManagerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\node\NodeInterface;
 use Drupal\node_edit_link\NodeCsrfTokenInterface;
@@ -21,7 +20,13 @@ use Symfony\Component\HttpFoundation\RequestStack;
 class NodeCsrfToken implements NodeCsrfTokenInterface {
 
   use StringTranslationTrait;
-  use MessengerTrait;
+
+  /**
+   * Core session manager service.
+   *
+   * @var \Drupal\Core\Session\SessionManagerInterface
+   */
+  protected $sessionManager;
 
   /**
    * Current request stack service.
@@ -68,7 +73,8 @@ class NodeCsrfToken implements NodeCsrfTokenInterface {
   /**
    * {@inheritDoc}
    */
-  public function __construct(RequestStack $request_stack, CsrfTokenGenerator $csrf_token, CacheBackendInterface $cache, MailManagerInterface $mail_manager, AccountProxyInterface $current_user, EmailValidatorInterface $mail_validator) {
+  public function __construct(SessionManagerInterface $session_manager, RequestStack $request_stack, CsrfTokenGenerator $csrf_token, CacheBackendInterface $cache, MailManagerInterface $mail_manager, AccountProxyInterface $current_user, EmailValidatorInterface $mail_validator) {
+    $this->sessionManager = $session_manager;
     $this->currentRequest = $request_stack->getCurrentRequest();
     $this->csrfToken = $csrf_token;
     $this->cache = $cache;
@@ -83,11 +89,24 @@ class NodeCsrfToken implements NodeCsrfTokenInterface {
   public function checkAccess(NodeInterface $node): bool {
     $current_request_query = $this->currentRequest->query;
     $mail_hash = $current_request_query->get('mail', FALSE);
+    $session = $this->currentRequest->getSession();
+    // If the user has already started the session for the current node, they
+    // should have access to it, until they submit the form.
+    if ($session->get('node_edit_link') == $node->id()) {
+      return TRUE;
+    }
+
     // The mail query parameter is used in conjunction with the node id to load
-    // the cache. Then the cached csrf token must match the token in the query
-    // to be allowed access to the node.
+    // the cache. Then the cached CSRF token must match the token in the query
+    // to be allowed access to the node. If all is satisfied, initiate a session
+    // for the user. This is necessary so that they will be able to successfully
+    // submit the node form.
     if ($mail_hash && $cache = $this->cache->get($this->getCid($node, $mail_hash))) {
-      return $cache->data['csrf'] == $current_request_query->get('edit-token');
+      if ($cache->data['csrf'] == $current_request_query->get('edit-token')) {
+        $this->sessionManager->start();
+        $this->currentRequest->getSession()->set('node_edit_link', $node->id());
+        return TRUE;
+      }
     }
     return FALSE;
   }
@@ -95,7 +114,7 @@ class NodeCsrfToken implements NodeCsrfTokenInterface {
   /**
    * {@inheritDoc}
    */
-  public function createCsrfToken(NodeInterface $node, $mail, int $expires = 604800): string {
+  public function createCsrfToken(NodeInterface $node, string $mail, int $expires = 604800): string {
     $token = $this->csrfToken->get(time() . $mail);
     // Create a token and save it in cache for reference later.
     $this->cache->set($this->getCid($node, $mail), ['csrf' => $token], time() + $expires, ['node_edit_link:' . $node->id()]);
@@ -114,26 +133,36 @@ class NodeCsrfToken implements NodeCsrfTokenInterface {
    */
   public function addFormElements(array &$form, FormStateInterface $form_state): void {
     $form['#attributes']['class'][] = 'centered-container';
+
+    if ($mail = $this->currentRequest->query->get('mail')) {
+      // Clear the access token since the user is now in the form. The session
+      // has been started at this time, and so we no longer need the token.
+      $this->clearCsrfToken($form_state->getBuildInfo()['callback_object']->getEntity(), $mail);
+      $form['revision_information']['#access'] = FALSE;
+    }
+
+    $form_component = $form_state->get('form_display')
+      ->getComponent('node_edit_link');
+    // The form component isn't added to the node form, we can escape.
+    if (empty($form_component)) {
+      return;
+    }
+
     $form['node_edit_link'] = [
       '#type' => 'details',
       '#title' => $this->t('One-Time Edit Link'),
       '#group' => 'advanced',
       '#tree' => TRUE,
       '#access' => $this->currentUser->hasPermission('send one time edit link'),
+      '#weight' => $form_component['weight'],
     ];
     $form['node_edit_link']['email'] = [
       '#type' => 'email',
       '#title' => $this->t('Email Address'),
       '#description' => $this->t('Send a one time edit link to the provided email address. Valid for 7 days'),
     ];
-    $form['actions']['submit']['#submit'][] = [$this, 'submitNodeForm'];
-
-    if ($mail = $this->currentRequest->query->get('mail')) {
-      // Store the mail hash query into storage so that we can clear the cached
-      // token upon submitting.
-      $form_state->set('node_edit_link', $mail);
-      $form['revision_information']['#access'] = FALSE;
-    }
+    $form['actions']['submit']['#submit'][] = [self::class, 'submitNodeForm'];
+    $form_state->set('node_edit_link', $this);
   }
 
   /**
@@ -143,46 +172,63 @@ class NodeCsrfToken implements NodeCsrfTokenInterface {
    *   Complete form.
    * @param \Drupal\Core\Form\FormStateInterface $form_state
    *   Submitted form state.
+   */
+  public static function submitNodeForm(array $form, FormStateInterface $form_state) {
+    /** @var NodeCsrfTokenInterface $node_csrf_token */
+    $node_csrf_token = $form_state->get('node_edit_link');
+
+    /** @var \Drupal\node\NodeInterface $node */
+    $node = $form_state->getBuildInfo()['callback_object']->getEntity();
+
+    // Send out the email to the user.
+    if ($email = $form_state->getValue(['node_edit_link', 'email'])) {
+      $email_body = $form_state->getValue(['node_edit_link', 'email_body']);
+      self::sendEmail($node, $node_csrf_token->createCsrfToken($node, $email), $email, $email_body);
+    }
+  }
+
+  /**
+   * Send out the email for the given node for one time editing.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   Node entity object.
+   * @param string $token
+   *   CSRF Token.
+   * @param string $mail
+   *   Send to Email address.
+   * @param string|null $message
+   *   Email body.
    *
    * @throws \Drupal\Core\Entity\EntityMalformedException
    */
-  public function submitNodeForm(array $form, FormStateInterface $form_state) {
-    /** @var \Drupal\node\NodeForm $node_form */
-    $node_form = $form_state->getBuildInfo()['callback_object'];
-    /** @var \Drupal\node\NodeInterface $node */
-    $node = $node_form->getEntity();
-    $email = $form_state->getValue(['node_edit_link', 'email']);
-
-    // First clear the current token that the user used.
-    if ($mail = $form_state->get('node_edit_link')) {
-      $this->clearCsrfToken($node, $mail);
-    }
-
-    if (empty($email)) {
-      return;
-    }
-
+  protected static function sendEmail(NodeInterface $node, string $token, string $mail, ?string $message = NULL) {
     // Construct an absolute url to email to the user with the appropriate query
     // parameters.
-    $url = $node_form->getEntity()->toUrl('edit-form', [
-      'query' => [
-        'edit-token' => $this->createCsrfToken($node, $email),
-        'mail' => self::getEmailHash($email),
-      ],
+    $url = $node->toUrl('edit-form', [
+      'query' => ['edit-token' => $token, 'mail' => self::getEmailHash($mail)],
       'absolute' => TRUE,
     ])->toString();
 
     $params = [
       'context' => [
-        'message' => 'This is a test attempt: ' . $url,
+        'message' => $message,
         'subject' => $node->label(),
       ],
     ];
 
+    $messenger = \Drupal::messenger();
+
     // Display a message to the current admin and send out the email to the
     // provided email address.
-    self::messenger()->addStatus($this->t('One time edit link: %link', ['%link' => $url]));
-    $this->mailManager->mail('system', $node->id(), $email, 'en', $params, NULL, TRUE);
+    $messenger->addStatus(t('One time edit link: %link', ['%link' => $url]));
+    try {
+      \Drupal::service('plugin.manager.mail')
+        ->mail('system', $node->id(), $mail, 'en', $params, NULL, TRUE);
+    }
+    catch (\Exception $e) {
+      // Do nothing. If the email fails to send, a message is already provided
+      // to the user.
+    }
   }
 
   /**

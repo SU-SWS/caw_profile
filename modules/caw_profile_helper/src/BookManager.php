69002e64982da99c88632e154ecada679a30e98f
Mike Decker <pookmish@gmail.com>
2022-03-23 15:11:47 -0700

    Simplified book manager method

--- modules/caw_profile_helper/src/BookManager.php
+++ modules/caw_profile_helper/src/BookManager.php
@@ -13,6 +13,13 @@ use Drupal\node\NodeInterface;
 class BookManager extends Manager {
 
   /**
+   * Boolean flag when loading all tree data.
+   *
+   * @var bool
+   */
+  protected $loadAllData = FALSE;
+
+  /**
    * Get the current page's book node or the current node if in the book.
    *
    * @param bool $return_current_page
@@ -48,36 +55,16 @@ class BookManager extends Manager {
    * Override Core's book method to display the entire book tree.
    */
   public function bookTreeAllData($bid, $link = NULL, $max_depth = NULL) {
+    $this->loadAllData = TRUE;
+    return parent::bookTreeAllData($bid, $link, $max_depth);
+  }
 
-    $tree = &drupal_static(__METHOD__, []);
-    $language_interface = \Drupal::languageManager()->getCurrentLanguage();
 
-    // Use $nid as a flag for whether the data being loaded is for the whole
-    // tree.
-    $nid = isset($link['nid']) ? $link['nid'] : 0;
-    // Generate a cache ID (cid) specific for this $bid, $link, $language, and
-    // depth.
-    $cid = 'book-links:' . $bid . ':all:' . $nid . ':' . $language_interface->getId() . ':' . (int) $max_depth;
-
-    if (!isset($tree[$cid])) {
-      // If the tree data was not in the static cache, build $tree_parameters.
-      $tree_parameters = [
-        'min_depth' => 1,
-        'max_depth' => $max_depth,
-      ];
-      if ($nid) {
-        $active_trail = $this->getActiveTrailIds($bid, $link);
-        // This commented out line allows the entire book to be returned.
-        // $tree_parameters['expanded'] = $active_trail;
-        $tree_parameters['active_trail'] = $active_trail;
-        $tree_parameters['active_trail'][] = $nid;
-      }
-
-      // Build the tree using the parameters; the resulting tree will be cached.
-      $tree[$cid] = $this->bookTreeBuild($bid, $tree_parameters);
-    }
-
-    return $tree[$cid];
+  /**
+   * {@inheritdoc}
+   */
+  public function getActiveTrailIds($bid, $link) {
+    return $this->loadAllData ? NULL : parent::getActiveTrailIds($bid, $link);
   }
 
 }

05a8c91bf51c0a3166435638213b9737769242ea
pookmish <pookmish@gmail.com>
2022-08-03 13:15:47 -0700

    Add some margin to the page top of sweeteners, improve comparison tool a11y (#91)

--- themes/stanford_basic/src/js/stanford_basic.behavior.js
+++ themes/stanford_basic/src/js/stanford_basic.behavior.js
@@ -10,8 +10,8 @@
 export default {
 
   // Attach Drupal Behavior.
-  attach(context, settings, once) {
-    (function ($) {
+  attach(context, settings) {
+    (function ($, once) {
       // Validate there is a skip link anchor for the main content. If not,
       // default to #page-content.
       var $mc = $('#main-content', context).length;
@@ -90,7 +90,7 @@ export default {
         }
       });
 
-    })(jQuery);
+    })(jQuery, once);
   },
 
   // Detach Example.

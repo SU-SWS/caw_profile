beec37f2ff336ea34fdf3b5ddb1a10ce0bba8a3b
Mike Decker <pookmish@gmail.com>
2022-08-03 15:17:28 -0700

    CAWD9-134 Card background color options

---
+++ modules/caw_profile_helper/src/Plugin/paragraphs/Behavior/CardColorBehavior.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\caw_profile_helper\Plugin\paragraphs\Behavior;
+
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\paragraphs\Entity\ParagraphsType;
+use Drupal\paragraphs\ParagraphInterface;
+use Drupal\paragraphs\ParagraphsBehaviorBase;
+
+/**
+ * Provides behaviors for card paragraphs to add color classes.
+ *
+ * @ParagraphsBehavior(
+ *   id = "card_color",
+ *   label = @Translation("Card Colors"),
+ *   description = @Translation("Change background colors.")
+ * )
+ */
+class CardColorBehavior extends ParagraphsBehaviorBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function isApplicable(ParagraphsType $paragraphs_type) {
+    return $paragraphs_type->id() == 'stanford_card';
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state) {
+    $form = parent::buildBehaviorForm($paragraph, $form, $form_state);
+
+    $form['bg_color'] = [
+      '#type' => 'select',
+      '#title' => t('Background Color'),
+      '#options' => [
+        'b1050e' => $this->t('Cardinal Red'),
+        '4d4f53' => $this->t('Cool Grey'),
+        'f4f4f4' => $this->t('Light Fog'),
+      ],
+      '#empty_option' => $this->t('None'),
+      '#default_value' => $paragraph->getBehaviorSetting('card_color', 'bg_color'),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function view(array &$build, Paragraph $paragraph, EntityViewDisplayInterface $display, $view_mode) {
+    if ($bg_color = $paragraph->getBehaviorSetting('card_color', 'bg_color')) {
+      $build['#attributes']['class'][] = "bg-$bg_color";
+    }
+  }
+
+}

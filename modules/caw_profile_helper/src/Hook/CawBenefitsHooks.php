69ec10fcc442e51e4a53247b24b942b54ef98c3b
Mike Decker <pookmish@stanford.edu>
2026-04-24 12:27:50 -0700

    Configured views for benefit detail and added some styles

--- modules/caw_profile_helper/src/Hook/CawBenefitsHooks.php
+++ modules/caw_profile_helper/src/Hook/CawBenefitsHooks.php
@@ -23,38 +23,9 @@ class CawBenefitsHooks {
   public function cawBenefitsNodeViewAlter(array &$build, NodeInterface $entity, EntityViewDisplayInterface $display) {
     if (
       $entity->bundle() != 'caw_benefits' ||
-      !$display instanceof LayoutBuilderEntityViewDisplay
+      $display->getMode() != 'default'
     ) {
       return;
-    }
-
-    /** @var \Drupal\layout_builder\Section $section */
-    foreach ($display->getSections() as $delta => $section) {
-      $fieldComponents = array_filter($section->getComponents(), fn(SectionComponent $component) => str_starts_with($component->getPluginId(), 'field_block:node:caw_benefits:'));
-
-      foreach ($fieldComponents as $component) {
-        $config = $component->get('configuration');
-        preg_match('/field_block:node:caw_benefits:(.*)$/', $config['id'], $matches);
-        if (
-          $entity->hasField($matches[1]) &&
-          !$entity->get($matches[1])->count()
-        ) {
-          unset($build['_layout_builder'][$delta]['main'][$component->getUuid()]);
-        }
-      }
-    }
-
-    foreach ($build['_layout_builder'] as $delta => $section) {
-      if (!isset($section['main'])) {
-        continue;
-      }
-      $componentUuid = key($section['main']);
-      if (
-        count($section['main']) == 1 &&
-        $section['main'][$componentUuid]['#plugin_id'] == 'jumpstart_ui_page_heading'
-      ) {
-        unset($build['_layout_builder'][$delta]);
-      }
     }
   }
 
---
+++ themes/caw_theme/lib/js/benefits.node.js
@@ -0,0 +1,53 @@
+(function ($, Drupal, once) {
+  Drupal.behaviors.cawBenefitsNode = {
+    attach: function attach(context, settings) {
+      const $expandAllButton = $('<button class="expand-all su-button--secondary">').html('Expand All<span class="visually-hidden"> details below</span>');
+      $expandAllButton.click(function () {
+        if ($expandAllButton.text().includes('Expand')) {
+          $('button[aria-expanded="false"]').click();
+          $expandAllButton.html('Collapse All<span class="visually-hidden"> details below</span>');
+        } else {
+          $('button[aria-expanded="true"]').click();
+          $expandAllButton.html('Expand All<span class="visually-hidden"> details below</span>');
+        }
+      });
+      $('.caw-benefits.plan-details h2').after($expandAllButton);
+
+      $('.views-element-container', context).each(function () {
+        $(this).closest('.views-block').addClass('accordion-block')
+        const $heading = $(this).find('h3').wrapInner('<span class="button-text"></span>');
+        const $accordionContent = $(this).find('.view.benefit-accordion');
+        if (!$accordionContent.length) {
+          return;
+        }
+
+        const id = $heading.text().toLowerCase().replaceAll(/[^a-z0-9-]/gi, '-');
+        $accordionContent.attr('id', id)
+          .attr('aria-labelledby', `${id}-heading`)
+          .attr('role', 'region')
+          .addClass('hidden');
+
+        $heading.attr('id', `${id}-heading`);
+        $heading.wrapInner('<button class="accordion-button"></button>');
+
+        const $button = $heading.find('button');
+        $button.append('<i class="fa-solid fa-plus"></i>');
+        $button.attr('aria-expanded', 'false')
+          .attr('aria-controls', id);
+
+        $button.on('click', function () {
+          $button.find('.fa-solid').toggleClass('fa-plus').toggleClass('fa-minus');
+          $accordionContent.toggleClass('hidden');
+
+          if ($button.attr('aria-expanded') === 'true') {
+            $button.attr('aria-expanded', 'false');
+          } else {
+            $button.attr('aria-expanded', 'true');
+          }
+        });
+
+      });
+    },
+  };
+})(jQuery, Drupal, once);
+
